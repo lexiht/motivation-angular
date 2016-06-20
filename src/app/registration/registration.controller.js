dfa2e58a22d2b8c4a40aa99b342ddeae6dfc3cac
letianw <tmac4ever0824@gmail.com>
2016-06-20 15:56:46 +1000

    change contributors to us

--- src/app/registration/registration.controller.js
+++ src/app/registration/registration.controller.js
@@ -1,20 +1,3 @@
-// angular.module("motivationAngular")
-//   .controller("RegistrationController", ["$scope", "$location", "$auth",function($scope, $location, $auth) {
-//       $scope.handleRegBtnClick = function() {
-//         $auth.submitRegistration($scope.registrationForm)
-//           .then(function() {
-//             $scope.$on('auth:registration-email-success', function(ev, message) {
-//               alert("A registration email was sent to " + message.email);
-//             });
-//           })
-//           .catch(function() {
-//             $scope.$on('auth:registration-email-error', function(ev, reason) {
-//               alert("Registration failed: " + reason.errors[0]);
-//             });
-//           });
-//       };
-//   }]);
-
 (function() {
   'use strict';
 
