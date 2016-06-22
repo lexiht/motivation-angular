0c2bad88213901f98a4eed34475e95de8addee7c
Robin Heathcote <robin.heathcote@icloud.com>
2016-06-22 19:20:06 +0100

    revert to gulp server

--- gulpfile.js
+++ gulpfile.js
@@ -27,3 +27,11 @@ wrench.readdirSyncRecursive('./gulp').filter(function(file) {
 gulp.task('default', ['clean'], function () {
   gulp.start('build');
 });
+
+gulp.task('serveprod', function() {
+  connect.server({
+    root: ["/"],
+    port: process.env.PORT || 5000, // localhost:5000
+    livereload: false
+  });
+});
