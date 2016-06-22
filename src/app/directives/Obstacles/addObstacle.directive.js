3dd0320099f83fae129097a1d35aa54ea1f53ed4
Robin Heathcote <robin.heathcote@icloud.com>
2016-06-22 11:54:52 +0100

    Add show button for obstacles-working

--- src/app/directives/Obstacles/addObstacle.directive.js
+++ src/app/directives/Obstacles/addObstacle.directive.js
@@ -23,16 +23,27 @@
 		/** @ngInject */
 		function addObstacleFormController($log, $scope, Restangular, $state) {
 			var vm = this;
+			vm.obstacles = [];
 
 			var allObstacles = Restangular.one("wishes",vm.wishid).all("obstacles");
 
 
 			vm.isAddObstacleOpen = false;
+			vm.isShowObstacleOpen = false;
 
 			Restangular.one('wishes', vm.wishid).get().then(function(resp) {
 				$log.log(resp);
 				vm.currentWish = resp;
 			});
+
+			allObstacles.getList().then(function(response){
+	      $log.log(response);
+	      response.forEach(function(obstacle){
+	        vm.obstacles.push(obstacle);
+					$log.log(vm.obstacles)
+	      });
+	    });
+
 
 			vm.addObstacle = function() {
 				allObstacles.post($scope.addObstacleForm);
--- src/app/wishes/wishes.controller.js
+++ src/app/wishes/wishes.controller.js
@@ -12,7 +12,7 @@ angular
     vm.obstacles = [];
 
     var allWishes = Restangular.all('wishes');
-    var allObstacles = Restangular.one("wishes",vm.wishid).all("obstacles");
+
 
     allWishes.getList().then(function(response){
       $log.log(response);
@@ -23,19 +23,9 @@ angular
       });
     });
 
-    allObstacles.getList().then(function(response){
-      $log.log(response);
-      response.forEach(function(obstacle){
-        // var singleWish = new wishFactory(wish.wish_text, wish.outcome_text);
-        // vm.wishes.push(singleWish);
-        vm.obstacles.push(obstacles);
-      });
-    });
-
     vm.createWish = function() {
       allWishes.post($scope.wishForm);
       $state.reload();
     };
-
-    }
-  })();
+  }
+})();
