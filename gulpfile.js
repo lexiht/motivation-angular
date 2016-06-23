c6a1b765f540268a9ccc19cafec52ed2e30d38a5
Robin Heathcote <robin.heathcote@icloud.com>
2016-06-23 10:24:16 +0100

    change of buildpack

--- gulpfile.js
+++ gulpfile.js
@@ -27,11 +27,3 @@ wrench.readdirSyncRecursive('./gulp').filter(function(file) {
 gulp.task('default', ['clean'], function () {
   gulp.start('build');
 });
-
-gulp.task('serveprod', function() {
-  connect.server({
-    root: ['/'],
-    port: this.server.listen(process.env.PORT || port),
-    livereload: false
-  });
-});
