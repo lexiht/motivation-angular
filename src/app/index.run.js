7005918a43b598aed6d23cea087e937cdb7e024b
letianw <tmac4ever0824@gmail.com>
2016-06-24 21:29:39 +1000

    now user can only visit both login and registration when not logged in

--- src/app/index.run.js
+++ src/app/index.run.js
@@ -8,7 +8,7 @@
   /** @ngInject */
   function runBlock($rootScope, $location, $state, UserService) {
     $rootScope.$on('$stateChangeStart', function(e, toState, toParams, fromState, fromParams){
-      var isLogin = toState.name === "login";
+      var isLogin = ((toState.name === "login") || (toState.name === "registration"));
         if(isLogin){
            return;
         }
